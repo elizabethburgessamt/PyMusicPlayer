3ea9dc8d5a4efd0761d163bf5bdfa1d15db48b25
elizabethburgessamt <elizabethburgessamt@gmail.com>
2017-12-19 11:54:10 -0500

    Get a basic display going along with adding placeholder sprites.

--- PyMusicPlayer/PyMusicPlayer/PyMusicPlayer.py
+++ PyMusicPlayer/PyMusicPlayer/PyMusicPlayer.py
@@ -1,6 +1,21 @@
 #import pygame.mixer
-import pygame
+import pygame, sys
+from pygame.locals import *
 
 pygame.init()
-window = pygame.display.set_mode((320, 204))
+window = pygame.display.set_mode((320, 240))
 screen = pygame.display.get_surface()
+
+btnPlay = pygame.image.load('img\sprPlayWhite.png')
+btnPause = pygame.image.load('img\sprPauseWhite.png')
+btnStop = pygame.image.load('img\sprStopWhite.png')
+
+while True:
+
+    window.blit(btnPlay, (50, 100))
+
+    for event in pygame.event.get():
+        if event.type == quit or (event.type == KEYUP and event.key == K_ESCAPE):
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()

f6265c154756e6dce54487dda2519c2566794a7c
elizabethburgessamt <elizabethburgessamt@gmail.com>
2017-12-20 12:03:51 -0500

    Add basic file list of music directory and mouse positoning to eventually click on button

--- PyMusicPlayer/PyMusicPlayer/PyMusicPlayer.py
+++ PyMusicPlayer/PyMusicPlayer/PyMusicPlayer.py
@@ -1,5 +1,5 @@
 #import pygame.mixer
-import pygame, sys
+import pygame, sys, os
 from pygame.locals import *
 
 pygame.init()
@@ -10,12 +10,34 @@ btnPlay = pygame.image.load('img\sprPlayWhite.png')
 btnPause = pygame.image.load('img\sprPauseWhite.png')
 btnStop = pygame.image.load('img\sprStopWhite.png')
 
-while True:
+debugMode = True
 
+if debugMode == True:
+    pthMusicDefault = 'C:\\Users\\Hectaimon\\Music'
+else:
+    pthMusicDefault = '\\media\\'
+
+def main():
+    filesMusic = [f for f in os.listdir(pthMusicDefault) if os.path.isfile(os.path.join(pthMusicDefault, f))]
+    
     window.blit(btnPlay, (50, 100))
 
-    for event in pygame.event.get():
-        if event.type == quit or (event.type == KEYUP and event.key == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+    mouseX = 0
+    mouseY = 0
+
+    while True:
+        mouseClicked = False
+        
+
+        for event in pygame.event.get():
+            
+            if event.type == quit or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONUP:
+                mouseX, mouseY = event.pos
+                mouseClicked = True
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()

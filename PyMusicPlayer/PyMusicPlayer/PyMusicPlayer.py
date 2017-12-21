a42e20ad6105bc39406e826fc71c7f3cca1886e7
elizabethburgessamt <elizabethburgessamt@gmail.com>
2017-12-21 12:05:45 -0500

    Revert "Made an interactive area as a "button" and methods to control music playback."
    This reverts commit ad65c5c16178220c8d3fc6b6ec1f22dba6690f02.

--- PyMusicPlayer/PyMusicPlayer/PyMusicPlayer.py
+++ PyMusicPlayer/PyMusicPlayer/PyMusicPlayer.py
@@ -21,7 +21,6 @@ def main():
     filesMusic = [f for f in os.listdir(pthMusicDefault) if os.path.isfile(os.path.join(pthMusicDefault, f))]
     
     window.blit(btnPlay, (50, 100))
-    btnActive = 'Stop'
 
     mouseX = 0
     mouseY = 0
@@ -38,36 +37,7 @@ def main():
             elif event.type == MOUSEBUTTONUP:
                 mouseX, mouseY = event.pos
                 mouseClicked = True
-        
-        if 50 < mouseX < 90 and 100 < mouseY < 140 and mouseClicked == True:
-            if btnActive == 'Stop':
-                btnActive = 'Play'
-                window.blit(btnPause, (50, 100))
-                loadSong(filesMusic)
-            elif btnActive == 'Play':
-                btnActive = 'Pause'
-                window.blit(btnPlay, (50, 100))
-                pauseSong()
-            elif btnActive == 'Pause':
-                btnActive = 'Play'
-                window.blit(btnPause, (50, 100))
-                loadSong(filesMusic)
         pygame.display.update()
-
-        
-        
-def loadSong(filesMusic):
-    #load song
-
-    #for f in filesMusic:
-    pygame.mixer.music.load(os.path.join(pthMusicDefault, filesMusic[1]))
-    pygame.mixer.music.play()
-
-def pauseSong():
-    pygame.mixer.music.pause()
-
-def stopSong():
-    pygame.mixer.music.stop()
 
 if __name__ == '__main__':
     main()

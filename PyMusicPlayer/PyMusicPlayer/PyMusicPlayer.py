ea9336af48cbcfd24070b31c160b16ca4d68f484
elizabethburgessamt <elizabethburgessamt@gmail.com>
2018-01-03 12:11:57 -0500

    Added Unpause for correct playback, surface fill so the buttons don't redraw over each other, and foundation for displaying text

--- PyMusicPlayer/PyMusicPlayer/PyMusicPlayer.py
+++ PyMusicPlayer/PyMusicPlayer/PyMusicPlayer.py
@@ -3,12 +3,15 @@ import pygame, sys, os
 from pygame.locals import *
 
 pygame.init()
+pygame.font.init()
 window = pygame.display.set_mode((320, 240))
 screen = pygame.display.get_surface()
 
 btnPlay = pygame.image.load('img\sprPlayWhite.png')
 btnPause = pygame.image.load('img\sprPauseWhite.png')
 btnStop = pygame.image.load('img\sprStopWhite.png')
+
+BASICFONT = pygame.font.Font(None, 12)
 
 debugMode = True
 
@@ -42,19 +45,24 @@ def main():
                 mouseX, mouseY = event.pos
                 mouseClicked = True
         
+        makeText(currentSong, Color(255, 255, 255, 255), Color(0, 0, 0, 0), 100, 100)
+
         if 50 < mouseX < 90 and 100 < mouseY < 140 and mouseClicked == True:
             if btnActive == 'Stop':
-                btnActive = 'Play'
+                window.fill(Color(0, 0, 0, 0))
                 window.blit(btnPause, (50, 100))
                 loadSong(filesMusic)
+                btnActive = 'Play'
             elif btnActive == 'Play':
                 btnActive = 'Pause'
+                window.fill(Color(0, 0, 0, 0))
                 window.blit(btnPlay, (50, 100))
                 pauseSong()
             elif btnActive == 'Pause':
-                btnActive = 'Play'
+                window.fill(Color(0, 0, 0, 0))
                 window.blit(btnPause, (50, 100))
-                playSong()
+                playSong(btnActive)
+                btnActive = 'Play'
         pygame.display.update()
 
         
@@ -66,14 +74,23 @@ def loadSong(filesMusic):
     #pygame.mixer.music.load(os.path.join(pthMusicDefault, filesMusic[1]))
     pygame.mixer.music.play()
 
-def playSong():
-    pygame.mixer.music.play()
+def playSong(btnActive):
+    if btnActive == "Pause":
+        pygame.mixer.music.unpause()
+    else:
+        pygame.mixer.music.play()
 
 def pauseSong():
     pygame.mixer.music.pause()
 
 def stopSong():
     pygame.mixer.music.stop()
+
+def makeText(text, color, bgcolor, top, left):
+    textSurf = BASICFONT.render(text, True, color, bgcolor)
+    textRect = textSurf.get_rect()
+    textRect = top, left
+    return (textSurf, textRect)
 
 if __name__ == '__main__':
     main()
